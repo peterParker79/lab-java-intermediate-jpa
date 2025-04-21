f7ce8d4613715089fb7f0dfd3e9e88ad3d023028
peterParker79 <trypeterparker@gmail.com>
2025-04-21 19:59:18 +0200

    initial commit

---
+++ src/main/java/com/ironhack/labIntermediateJPA/LabIntermediateJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.labIntermediateJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabIntermediateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabIntermediateJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/labIntermediateJPA/LabIntermediateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.labIntermediateJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabIntermediateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
